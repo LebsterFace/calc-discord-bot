7b4dd93e0c77a142efa46921f731bc3e36f49961
LebsterFace <44923609+LebsterFace@users.noreply.github.com>
2019-08-16 02:10:47 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,12 @@
+const Discord=require("discord.js"),
+	BigEval = require("bigeval");
+	client = new Discord.Client();
+client.on("ready", () => {
+  console.log("CalcBot Online!");
+});
+const cl = x=>new BigEval.exec(x);
+client.on("message", message => {
+  if (message.author.bot) return;
+  message.channel.send(`\`\`\`${cl(message.content)}\`\`\``);
+});
+client.login(process.env.BOT_TOKEN);
